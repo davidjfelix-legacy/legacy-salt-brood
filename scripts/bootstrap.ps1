4f3e1a5c7b88b15d22af14dd9a6a777e0a69114f
David J. Felix <felix.davidj@gmail.com>
2014-11-17 11:39:49 -0500

    Create bootstrap.ps1

---
+++ scripts/bootstrap.ps1
@@ -0,0 +1,11 @@
+#Has Proxy?
+  #Get Proxy
+  #Needs Auth?
+    #Get username
+    #Get password
+
+#64/32bit
+#Download curl in correct bit
+#Run msi
+#add to path directory
+#ensure path directory in path
