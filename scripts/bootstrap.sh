7c29269f1e82b9c43501fe4a564a3b395c9e843a
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-07-05 19:08:03 -0400

    Add basic functionality for 15.04 to bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,16 @@
+# FIXME: remove reliance on sudo
+
 # get salt
-# get these files
-# run salt
+# FIXME: security vulnerability
 curl -L https://bootstrap.saltstack.com | sudo sh
+
+# FIXME: make this platform independent
+sudo apt-get -y install python-git git
+
+# Place my brood files in srv... TODO: determine if this dir is right for masterless
+# FIXME: add interaction to determine which repo
+sudo mkdir -p /srv/salt
+sudo git clone https://github.com/hatchery/my-brood /srv/salt/my-brood
+
+cd /srv/salt/my-brood
 sudo salt-call --local --config-dir=./ state.highstate
