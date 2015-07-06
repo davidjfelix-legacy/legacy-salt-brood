a84eb3fcd9ad102994381c8cbbb04a6aabda7a7a
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-07-05 20:09:58 -0400

    We run masterless here; remove salt-minion daemon

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,6 +3,8 @@
 # get salt
 # FIXME: security vulnerability
 curl -L https://bootstrap.saltstack.com | sudo sh
+sudo systemctl stop salt-minion
+sudo systemctl disable salt-minion
 
 # FIXME: make this platform independent
 sudo apt-get -y install python-git git
