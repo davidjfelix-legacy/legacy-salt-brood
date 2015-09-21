50d4bd75ec301ea531bec6547805b35ebd228e91
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-09-21 14:28:04 -0400

    Update bootstrap.upstart.sh

--- scripts/bootstrap.upstart.sh
+++ scripts/bootstrap.upstart.sh
@@ -5,11 +5,12 @@ set -o nounset
 
 # get salt
 # FIXME: make this platform independent or change script name
-sudo apt-get update
-sudo apt-get -y install python-git git python-software-properties software-properties-common
-sudo add-apt-repository -y ppa:saltstack/salt
-sudo apt-get update
-sudo apt-get -y install salt-minion
+# FIXME: remove all the sudo -E by passing the environment through in sudoers
+sudo -E apt-get update
+sudo -E apt-get -y install python-git git python-software-properties software-properties-common
+sudo -E add-apt-repository -y ppa:saltstack/salt
+sudo -E apt-get update
+sudo -E apt-get -y install salt-minion
 sudo service salt-minion stop
 
 # Place my brood files in srv... TODO: determine if this dir is right for masterless
@@ -17,7 +18,7 @@ sudo service salt-minion stop
 sudo mkdir -p /srv/salt
 sudo rm -rf /srv/salt/my-brood
 cd /srv/salt/
-sudo git clone https://github.com/hatchery/my-brood /srv/salt/my-brood
+sudo -E git clone https://github.com/hatchery/my-brood /srv/salt/my-brood
 
 cd /srv/salt/my-brood
 sudo salt-call --local --config-dir=./ state.highstate
